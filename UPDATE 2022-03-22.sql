9bd7b94f322d649dc816bea49c08494f6ff00f0d
German Bertolini <german.bertolini@gmail.com>
2022-03-22 22:07:09 -0300

    Fix spPrecioGet

--- UPDATE 2022-03-22.sql
+++ UPDATE 2022-03-22.sql
@@ -238,3 +238,23 @@ BEGIN
 END
 
 GO
+
+ALTER PROCEDURE [dbo].[spPrecioGet]
+(
+	@ListaDePreciosId INT = NULL,
+	@ProductoId INT
+)
+AS
+BEGIN
+
+	SELECT top 100 --parche 21-03-22 para poder listar
+		ProductoPrecioId,
+		Precio,
+		ListaDePreciosId
+	FROM
+		vwPreciosVigentes PV
+	WHERE
+		PV.ListaDePreciosId = COALESCE(@ListaDePreciosId, PV.ListaDePreciosId)
+		AND PV.ProductoId = @ProductoId;
+
+END
